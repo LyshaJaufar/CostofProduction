80328eccabdb728517a6a941192947c55d1e2a65
LyshaJaufar <lysha.ahmd@gmail.com>
2021-01-14 05:07:19 +0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import argv, exit
+import csv
 
 # Check for command-line args
 if len(argv) != 2:
@@ -8,12 +9,32 @@ if len(argv) != 2:
 # Open the csv file
 database = open(argv[1], "r")
 
+# Create DictReader
+reader = csv.DictReader(database)
+
+# Prompt for FC
+fixedCost = int(input("Enter fixed cost: "))
+
+# Iterate over CSV file, printing each title
+for row in reader:
+    FC = row["Total fixed costs"]
+    
+    if FC in row:
+        database.append(fixedCost)
+        print(FC)
+
 # 1.Create a dictionary of the database (read contents into memory)
 #   Create list of the particular STRs
 STRs = []
+
 person = {}
 for name, data in enumerate(database):
     if name == 0:
         # Generate list of STR using for loop
         STRs = [x for x in data.strip().split(",")][1:]  # output not included
+        FC = name["Total fixed costs"]
         print(STRs)
+    else:
+        if FC in row:
+            database.append(fixedCost)
+            print(FC)
